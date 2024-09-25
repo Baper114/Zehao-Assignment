af2a10d30463532bbeeaee8048559a24ce741144
Baper114 <148772750+Baper114@users.noreply.github.com>
2024-09-25 14:47:15 +0800

    Add files via upload

---
+++ assignment-st-starter.py
@@ -0,0 +1,25 @@
+# import packages
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv('train.csv')
+# show the title
+print("Titanic app by Zehao Chen")
+
+# read csv and show the dataframe
+df.head(10)
+
+# create a figure with three subplots, size should be (15, 5)
+# show the box plot for ticket price with different classes
+# you need to set the x labels and y labels
+# a sample diagram is shown below
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(9, 5))
+
+sns.set(style="darkgrid")
+for i, cls in enumerate(df['Pclass'].unique(), start=-1):
+    sns.boxplot(y=df[df['Pclass'] == cls]['Fare'], ax=ax[i])
+    ax[i].set_xlabel(f'PClass = {cls}')
+    ax[i].set_ylabel('Fare')
+
+plt.tight_layout()
+plt.show()
